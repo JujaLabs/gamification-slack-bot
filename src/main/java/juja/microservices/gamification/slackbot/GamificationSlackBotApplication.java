149b1053a22baa9a6401a98a4ea6a1cfc4e4ffbd
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2017-02-27 22:51:07 +0200

    #1: add init structure

---
+++ src/main/java/juja/microservices/gamification/slackbot/GamificationSlackBotApplication.java
@@ -0,0 +1,15 @@
+package juja.microservices.gamification.slackbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Danil Kuznetsov
+ */
+
+@SpringBootApplication
+public class GamificationSlackBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GamificationSlackBotApplication.class);
+    }
+}
