b347d1f8b5fdc80a7896710d0a2829a2231782ce
NikolayNN <Horushko.Nikolay@gmail.com>
2017-03-08 23:52:16 +0300

    optimize imports

--- src/main/java/juja/microservices/gamification/slackbot/model/CodenjoyAchievment.java
+++ src/main/java/juja/microservices/gamification/slackbot/model/CodenjoyAchievment.java
@@ -1,7 +1,5 @@
 package juja.microservices.gamification.slackbot.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 /**
