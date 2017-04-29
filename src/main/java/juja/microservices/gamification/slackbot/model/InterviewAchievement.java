f4be7eed9cf1fc78c6edf1a52bcf4c867f2bc557
NikolayNN <Horushko.Nikolay@gmail.com>
2017-04-29 19:43:20 +0300

    #37 : delete unused methods in the classes

--- src/main/java/juja/microservices/gamification/slackbot/model/InterviewAchievement.java
+++ src/main/java/juja/microservices/gamification/slackbot/model/InterviewAchievement.java
@@ -9,8 +9,6 @@ import lombok.ToString;
  */
 
 @Getter
-@Setter
-@ToString
 public class InterviewAchievement {
 
     private String from;
--- src/main/java/juja/microservices/gamification/slackbot/model/ThanksAchievement.java
+++ src/main/java/juja/microservices/gamification/slackbot/model/ThanksAchievement.java
@@ -8,8 +8,6 @@ import lombok.ToString;
  * @author Vitalii Viazovoi
  */
 @Getter
-@Setter
-@ToString
 public class ThanksAchievement {
     private String from;
     private String to;
