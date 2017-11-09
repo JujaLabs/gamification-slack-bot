d14c935e45822dabe04e9df708b95b41690d9042
Ivan Shapovalov <ivshapovalov@gmail.com>
2017-11-09 20:38:29 +0300

    #105 Update Team repo unit tests with Feign client

--- src/test/java/juja/microservices/gamification/slackbot/dao/RestTeamRepositoryTest.java
+++ src/test/java/juja/microservices/gamification/slackbot/dao/RestTeamRepositoryTest.java
@@ -1,19 +1,16 @@
 package juja.microservices.gamification.slackbot.dao;
 
+import feign.FeignException;
+import juja.microservices.gamification.slackbot.dao.feign.TeamsClient;
 import juja.microservices.gamification.slackbot.exceptions.TeamExchangeException;
 import juja.microservices.gamification.slackbot.model.DTO.TeamDTO;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.web.client.RestTemplate;
 
 import javax.inject.Inject;
 import java.util.Arrays;
@@ -21,63 +18,87 @@ import java.util.LinkedHashSet;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withBadRequest;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+/**
+ * @author Petr Kramar
+ * @author Ivan Shapovalov
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RestTeamRepositoryTest {
-
-    @Inject
-    private TeamRepository teamRepository;
-
-    @Inject
-    private RestTemplate restTemplate;
-
-    private MockRestServiceServer mockServer;
-
-    @Value("${teams.endpoint.teamByUserUuid}")
-    private String teamsGetTeamByUserUuidUrl;
-
     @Rule
     public ExpectedException thrown = ExpectedException.none();
-
-    @Before
-    public void setup() {
-        mockServer = MockRestServiceServer.bindTo(restTemplate).build();
-    }
+    @Inject
+    private TeamRepository teamRepository;
+    @MockBean
+    private TeamsClient teamsClient;
 
     @Test
-    public void getTeam() {
+    public void findTeamByUserUuidReturnsCorrectTeam() {
         //given
-        mockServer.expect(requestTo(teamsGetTeamByUserUuidUrl + "/uuid1"))
-                .andExpect(method(HttpMethod.GET))
-                .andRespond(withSuccess(
-                        "{\"id\":\"123\",\"fakefield\":\"blablabla\",\"members\":[\"uuid1\",\"uuid2\",\"uuid3\",\"uuid4\"]}",
-                        MediaType.APPLICATION_JSON));
-        TeamDTO expectedTeam = new TeamDTO(new LinkedHashSet<>(Arrays.asList("uuid1", "uuid2", "uuid3", "uuid4")));
+        TeamDTO expected = new TeamDTO(new LinkedHashSet<>(Arrays.asList("uuid1", "uuid2", "uuid3", "uuid4")));
+        when(teamsClient.getTeamByUserUuid("uuid1")).thenReturn(expected);
 
         //when
-        TeamDTO result = teamRepository.getTeamByUserUuid("uuid1");
+        TeamDTO actual = teamRepository.getTeamByUserUuid("uuid1");
 
         //then
-        assertEquals(expectedTeam.getMembers(), result.getMembers());
+        assertEquals(expected.getMembers(), actual.getMembers());
+        verify(teamsClient).getTeamByUserUuid("uuid1");
+        verifyNoMoreInteractions(teamsClient);
     }
 
     @Test
-    public void shouldThrowExceptionWhenTeamRepositoryThrowException() {
+    public void findTeamByUserUuidWhenTeamServerReturnsFeignExceptionWithCorrectContent() {
         //given
-        mockServer.expect(requestTo(teamsGetTeamByUserUuidUrl + "/uuid1"))
-                .andExpect(method(HttpMethod.GET))
-                .andRespond(withBadRequest().body("bad request"));
+        String expectedJsonResponseBody =
+                "status 400 reading TeamsClient#getTeamByUserUuid(uuid1); content:" +
+                        "{\n" +
+                        "  \"httpStatus\": 400,\n" +
+                        "  \"internalErrorCode\": \"TMF-F2-D2\",\n" +
+                        "  \"clientMessage\": \"You cannot get/deactivate team if the user not a member of any team!\",\n" +
+                        "  \"developerMessage\": \"The reason of the exception is that user not in team\",\n" +
+                        "  \"exceptionMessage\": \"The reason of the exception is that user not in team\",\n" +
+                        "  \"detailErrors\": []\n" +
+                        "}";
+        FeignException feignException = mock(FeignException.class);
+        when(teamsClient.getTeamByUserUuid("uuid1")).thenThrow(feignException);
+        when(feignException.getMessage()).thenReturn(expectedJsonResponseBody);
 
-        //then
         thrown.expect(TeamExchangeException.class);
+        thrown.expectMessage(containsString("You cannot get/deactivate team if the user not a member of any team!"));
+
+        try {
+            //when
+            teamRepository.getTeamByUserUuid("uuid1");
+        } finally {
+            //then
+            verify(teamsClient).getTeamByUserUuid("uuid1");
+            verifyNoMoreInteractions(teamsClient);
+        }
+    }
+
+    @Test
+    public void findTeamByUserUuidWhenTeamServerReturnsFeignExceptionWithIncorrectContent() {
+        //given
+        RuntimeException runtimeException =
+                new RuntimeException("I'm, sorry. I cannot parse api error message from remote service :(");
+        when(teamsClient.getTeamByUserUuid("uuid1")).thenThrow(runtimeException);
+
+        thrown.expect(RuntimeException.class);
         thrown.expectMessage(containsString("I'm, sorry. I cannot parse api error message from remote service :("));
 
-        //when
-        teamRepository.getTeamByUserUuid("uuid1");
+        try {
+            //when
+            teamRepository.getTeamByUserUuid("uuid1");
+        } finally {
+            //then
+            verify(teamsClient).getTeamByUserUuid("uuid1");
+            verifyNoMoreInteractions(teamsClient);
+        }
     }
 }
