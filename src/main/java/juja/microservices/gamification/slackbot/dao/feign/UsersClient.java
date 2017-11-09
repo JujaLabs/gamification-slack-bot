091395718162d6cffd7bb7efb2aae1fd6f964df1
Ivan Shapovalov <ivshapovalov@gmail.com>
2017-11-09 20:30:26 +0300

    #105 Create User Client with Feign interface

---
+++ src/main/java/juja/microservices/gamification/slackbot/dao/feign/UsersClient.java
@@ -0,0 +1,26 @@
+package juja.microservices.gamification.slackbot.dao.feign;
+
+import juja.microservices.gamification.slackbot.model.DTO.SlackNameRequest;
+import juja.microservices.gamification.slackbot.model.DTO.UuidRequest;
+import juja.microservices.gamification.slackbot.model.DTO.UserDTO;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Ivan Shapovalov
+ */
+@FeignClient(name = "gateway")
+public interface UsersClient {
+    @RequestMapping(method = RequestMethod.POST, value = "/v1/users/usersBySlackNames",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    List<UserDTO> findUsersBySlackNames(SlackNameRequest request);
+
+    @RequestMapping(method = RequestMethod.POST, value = "/v1/users/usersByUuids",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    Set<UserDTO> findUsersByUuids(UuidRequest request);
+}
